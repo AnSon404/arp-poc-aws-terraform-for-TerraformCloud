87ffa4e06d8115886b976e586f34a8ae531f1839
AnSon404 <pps023002_2005@yahoo.com.hk>
2023-07-06 16:49:35 +0800

    add terraform-aws-hashicorp-vault-eks-addon module

---
+++ modules/kubernetes-addons/terraform-aws-hashicorp-vault-eks-addon/outputs.tf
@@ -0,0 +1,9 @@
+output "argocd_gitops_config" {
+  description = "Configuration used for managing the add-on with ArgoCD"
+  value       = var.manage_via_gitops ? { enable = true } : null
+}
+
+output "merged_helm_config" {
+  description = "(merged) Helm Config for HashiCorp Vault"
+  value       = helm_release.vault
+}
---
+++ modules/kubernetes-addons/terraform-aws-hashicorp-vault-eks-addon/variables.tf
@@ -0,0 +1,11 @@
+variable "helm_config" {
+  description = "HashiCorp Vault Helm chart configuration"
+  type        = any
+  default     = {}
+}
+
+variable "manage_via_gitops" {
+  description = "Determines if the add-on should be managed via GitOps"
+  type        = bool
+  default     = false
+}
